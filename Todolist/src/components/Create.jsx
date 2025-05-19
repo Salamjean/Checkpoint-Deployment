30846d7a4e53c097c5b837c76bd0d25fa4f1befd
SalamJean <salamjeanlouis8@gmail.com>
2025-05-19 18:34:50 +0000

    fix: end

--- Todolist/src/components/Create.jsx
+++ Todolist/src/components/Create.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 
 const Create =()=>{
   const [task, setTask] = useState();
   const handleAdd =()=>{
-     axios.post('http://localhost:3001/add', {task: task})
+     axios.post('https://deployement-server.onrender.com/add', {task: task})
      .then(result => {
       location.reload()
      })
--- Todolist/src/components/Home.jsx
+++ Todolist/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs';
 import Create from './Create'
@@ -6,13 +6,13 @@ import Create from './Create'
 const Home =()=>{
     const [todos, setTodos] = useState([]);
     useEffect(()=>{
-        axios.get('http://localhost:3001/get')
+        axios.get('https://deployement-server.onrender.com/get')
         .then(result => setTodos(result.data))
         .catch(err => console.log(err))
     },[])
 
     const handleEdit =(id)=>{
-        axios.put('http://localhost:3001/update/'+id)
+        axios.put('https://deployement-server.onrender.com/update/'+id)
         .then(result => {
             location.reload()
         })
@@ -20,7 +20,7 @@ const Home =()=>{
     }
 
     const handleDelete =(id)=>{
-        axios.delete('http://localhost:3001/delete/'+id)
+        axios.delete('https://deployement-server.onrender.com/delete/'+id)
         .then(result => {
             location.reload()
         })
